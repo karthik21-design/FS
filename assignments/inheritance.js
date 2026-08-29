f1759e1592867a26668de82dbf3316d417e20e3d
karthik21-design <chellurikarthik2025@gmail.com>
2026-08-29 17:11:27 +0530

    assignment updated

--- assignments/inheritance.js
+++ assignments/inheritance.js
@@ -1,106 +1,143 @@
-//Single Inheritance
-console.log("Single Inheritance - inheritance.js:2");
+// SINGLE INHERITANCE
+console.log("===== Single Inheritance =====");
+
 class Animal {
-    constructor(name){
-        this.name=name;
+    constructor(name) {
+        this.name = name;
     }
-    eat(){
-        console.log(`${this.name} is eating. - inheritance.js:8`);
+
+    eat() {
+        console.log(`${this.name} is eating.`);
     }
 }
-class Dog extends Animal{
-    bark(){
-        console.log(`${this.name} is barking. - inheritance.js:13`)
-    } 
+
+class Dog extends Animal {
+    bark() {
+        console.log(`${this.name} is barking.`);
+    }
 }
-const myDog = new Dog("Buddy");
+
+const myDog = new Dog("Rocky");
 myDog.eat();
 myDog.bark();
 
-//Multilevel Inheritance
-console.log("Multilevel Inheritance - inheritance.js:21");
-class Vehicle {
-    move(){
-        console.log("Moving forward - inheritance.js:24");
-    }
-}
-class Bike extends Vehicle{
-    wheels(){
-        console.log("Bike has 2 Wheels - inheritance.js:29");
-    }
-}
-class Car extends Bike{
-    windows(){
-        console.log("Car has 4 Windows - inheritance.js:34");
-    }
-}
-const v = new Car();
-v.move();
-v.wheels();
-v.windows();
 
-//Hierarchial Inheritance
-console.log("Hierarchial Inheritance - inheritance.js:43");
-class User{
-    constructor(username){
+// MULTILEVEL INHERITANCE
+console.log("\n===== Multilevel Inheritance =====");
+
+class Vehicle {
+    move() {
+        console.log("Moving forward.");
+    }
+}
+
+class Bike extends Vehicle {
+    wheels() {
+        console.log("Bike has 2 wheels.");
+    }
+}
+
+class Car extends Bike {
+    windows() {
+        console.log("Car has 4 windows.");
+    }
+}
+
+const myCar = new Car();
+myCar.move();
+myCar.wheels();
+myCar.windows();
+
+
+// HIERARCHICAL INHERITANCE
+console.log("\n===== Hierarchical Inheritance =====");
+
+class User {
+    constructor(username) {
         this.username = username;
     }
-    login(){
-        console.log(`${this.username} logged in. - inheritance.js:49`);
-    }
-}
-class Admin extends User{
-    deleteUser(){
-        console.log(`${this.username} deleted by admin - inheritance.js:54`);
-    }
-}
-class Customer extends User{
-    checkout(){
-        console.log(`${this.username} checked out successfully. - inheritance.js:59`);
-    }
-}
-const a = new Admin("Anvitha");
-const c = new Customer("Sam");
-a.login();
-c.login();
-a.deleteUser();
-c.checkout();
 
-//Multiple Inheritance
-console.log("Multiple Inheritance - inheritance.js:70");
-//Using Mixins
-const hall = {
-    sofa(){
-        console.log("Sofa is placed in Hall. - inheritance.js:74");
-    },
-    tv(){
-        console.log("Tv is placed in Hall. - inheritance.js:77")
+    login() {
+        console.log(`${this.username} logged in.`);
     }
-}; 
-const bedRoom = {
-    bed(){
-        console.log("Bed is in bedroom. - inheritance.js:82");
+}
+
+class Admin extends User {
+    deleteUser() {
+        console.log(`${this.username} deleted a user.`);
+    }
+}
+
+class Customer extends User {
+    checkout() {
+        console.log(`${this.username} checked out successfully.`);
+    }
+}
+
+const admin = new Admin("Karthik");
+const customer = new Customer("Ram");
+
+admin.login();
+customer.login();
+admin.deleteUser();
+customer.checkout();
+
+// MULTIPLE INHERITANCE (MIXINS)
+console.log("\n===== Multiple Inheritance (Using Mixins) =====");
+
+// Mixin 1
+const Hall = {
+    sofa() {
+        console.log("Sofa is placed in Hall.");
     },
-    closet(){
-        console.log("Closet is in Bedroom. - inheritance.js:85");
+
+    tv() {
+        console.log("TV is placed in Hall.");
     }
 };
-class Building{
-    constructor(houseNo){
+
+// Mixin 2
+const BedRoom = {
+    bed() {
+        console.log("Bed is in Bedroom.");
+    },
+
+    closet() {
+        console.log("Closet is in Bedroom.");
+    }
+};
+
+class Building {
+    constructor(houseNo) {
         this.houseNo = houseNo;
     }
-    house(){
-        console.log(`Building House No: ${this.houseNo} - inheritance.js:93`);
+
+    house() {
+        console.log(`Building House No: ${this.houseNo}`);
     }
 }
-class Area extends Building{
-    constructor(houseNo,area){
+
+class Area extends Building {
+    constructor(houseNo, area) {
         super(houseNo);
         this.area = area;
     }
+
+    showArea() {
+        console.log(`Area: ${this.area}`);
+    }
 }
-Object.assign(Area.prototype,hall,bedRoom);
-const name = new Area("403","Kommadi");
-name.house();
-name.sofa();
-name.bed();
+
+// Apply Mixins
+Object.assign(Area.prototype, Hall, BedRoom);
+
+const myArea = new Area("101", "MVP Colony");
+
+myArea.house();
+myArea.showArea();
+myArea.sofa();
+myArea.tv();
+myArea.bed();
+myArea.closet();
+
+
